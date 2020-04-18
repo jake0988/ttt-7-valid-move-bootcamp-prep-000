6843f34250c28dbac6ce936d8d8965eb9d15f9fc
Jake G <jake098877@gmail.com>
2020-04-18 22:30:14 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,10 +2,10 @@
 def valid_move?(board, index)
   if (board[index] == " " || board[index] == "" || board[index] == nil || index < 0 || index > 8))
   false
-  if (index.between?(0, 8) && (board[index] == "X" || board[index] == "O"))
+  elsif (index.between?(0, 8) && (board[index] == "X" || board[index] == "O"))
     true
 
-  elsif ()
+  else ()
     false
   end
 end
