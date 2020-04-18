fd2ce7fac7b2bbfd67471533576bf096fb575fbb
Jake G <jake098877@gmail.com>
2020-04-18 23:15:57 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,12 +2,11 @@
 def valid_move?(board, index)
   if (((board[index + 1] != "X" || board[index + 1] == "O") && index != 0) || index > 8 || index < 0)
     false
-  elsif  (index.between?(0, 8))
-    true
 
   elsif (position_taken?(board, index) == false)
     false
-
+  else
+    true
 
   end
 end
